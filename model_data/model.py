13e67c3a5f3ca9f0699456e93bb62f4856b5f856
guodong <3130104253@zju.edu.cn>
2018-12-17 21:11:06 +0800

    add conv

--- model_data/model.py
+++ model_data/model.py
@@ -1,11 +1,30 @@
+# coding=utf8
+
 import tensorflow as tf
-import numpy as tf
+import numpy as np
 
 
-def conv(inputs, filters, ):
-    x = tf.layers.conv2d(inputs=inputs, filters=filters)
-    print(23)
+def conv_block(inputs, filters, kernels, strides, use_bias):
+    """
+    :param use_bias:
+    :param inputs: shape=[batch_size,w,h,c]
+    :param filters: int
+    :param kernels: w*h
+    :param strides: w*h
+
+    :return:
+    """
+    x = tf.layers.conv2d(inputs=inputs, filters=filters, kernel_size=kernels, strides=strides, padding="same",
+                         use_bias=use_bias)
+    x = tf.layers.batch_normalization(x)
+    x = tf.nn.leaky_relu(x, alpha=0.2)
+
+    return x
 
 
 def encoder(x):
+    with tf.name_scope("encoder"):
+        with tf.name_scope("scale"):
+            x = conv_block(x, filters=64, kernels=(3, 3), strides=())
+
     return
