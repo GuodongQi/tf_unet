15a2bcbec344dbd63a17d87e31ce57e76186aa9f
齐国栋 <guodong_qi@zju.edu.cn>
2018-12-18 20:35:07 +0800

    update train

--- model_data/model.py
+++ model_data/model.py
@@ -19,7 +19,7 @@ def conv_block(inputs, filters_unm, kernels, strides, use_bias):
         x = tf.layers.batch_normalization(x)
     else:
         x = tf.layers.conv2d(inputs=inputs, filters=filters_unm, kernel_size=kernels, strides=strides, padding="same",
-                             use_bias=use_bias, bias_initializer=tf.truncated_normal_initializer())
+                             use_bias=use_bias)
     x = tf.nn.leaky_relu(x, alpha=0.2)
 
     return x
@@ -96,19 +96,24 @@ def u_net(input_data):
         up1 = tf.concat([up1, input_data], 3)
 
     with tf.name_scope("head"):
-        x = conv_block(up1, filters_unm=64, kernels=(3, 3), strides=(2, 2), use_bias=True)  # 216*64
+        x = conv_block(up1, filters_unm=64, kernels=(3, 3), strides=(2, 2), use_bias=True)  # 64*216
         x = conv_block(x, filters_unm=128, kernels=(3, 3), strides=(2, 2), use_bias=False)
         x = conv_block(x, filters_unm=256, kernels=(3, 3), strides=(2, 2), use_bias=True)
-        x = conv_block(x, filters_unm=512, kernels=(3, 3), strides=(2, 2), use_bias=False)  # 32 * 8 * 512
+        x = conv_block(x, filters_unm=512, kernels=(3, 3), strides=(2, 2), use_bias=False)  # 8 * 32 * 512
 
     with tf.name_scope("fc"):
-        x = tf.layers.conv2d(x, filters=1, kernel_size=(13, 8), strides=(1, 1), padding='valid',
-                             use_bias=True)  # batch*20 * 1 * 1
-        x = x[..., 0, 0]
+        x = tf.layers.conv2d(x, filters=1, kernel_size=(8, 13), strides=(1, 1), padding='valid',
+                             use_bias=False)  # batch*1 * 20 * 1
+        # x = tf.nn.softmax(x)
+        x = x[:, 0, :, 0]
         return x
 
 
 def eva_loss(label, predict):
     """eval loss"""
-    loss = tf.losses.softmax_cross_entropy(label, predict)
-    return loss
+    # predict = tf.
+    loss = tf.losses.mean_squared_error(labels=label, predictions=predict)
+    all_vars = tf.trainable_variables()
+    loss_l2 = tf.add_n([tf.nn.l2_loss(v) for v in all_vars
+                        if 'bias' not in v.name]) * 0.001
+    return loss + loss_l2
--- unet.py
+++ unet.py
@@ -2,70 +2,120 @@
 
 import tensorflow as tf
 import random
-from model_data.model import *
-from utils.utils import *
-import cv2
-import os
-import numpy as np
+
+from model_data.model import u_net, eva_loss
+from utils.utils import tf_read_image
 
 
 class UNET:
     def __init__(self):
         self.image_path = './data/train.txt'
-        self.batch_size = 2
+        self.batch_size = 16
 
         self.image_size = [500, 100]
         self.label_size = 20
         self.epoch_num = 50
+        self.learning_rate = 0.001
 
-        self.num = 0  # the nth of line
+        self.val_split = 0.1
 
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=config)
 
+        self.num = 0  # the nth of line
         # load data
         with open(self.image_path) as f:
-            self.lines = f.readlines()
-        random.shuffle(self.lines)
-        self.eval()
+            self.lines = f.readlines()[:300]
+            split_num = int(self.val_split * len(self.lines))
+            self.train_set = self.lines[split_num:]
+            self.val_set = self.lines[:split_num]
 
-    def load_data(self):
+        self.train()
+
+    def load_data(self, train_set):
         """generate a batch data"""
         img_set = []
         label_set = []
-        for i in range(self.batch_size):
-            if self.num + self.batch_size > len(self.lines):
-                pdata = self.lines[len(self.lines) - i]
-            else:
-                pdata = self.lines[self.num + i]
 
+        for i in range(self.batch_size):
+            if self.num + self.batch_size >= len(train_set):
+                pdata = train_set[len(train_set) - 1 - i]
+                random.shuffle(self.train_set)
+            else:
+                pdata = train_set[self.num + i]
             img, label = pdata.split()
             img_set.append("./data/picture/" + img + ".jpg")
             label_set.append(list(map(eval, list(label))))
         self.num += self.batch_size
         return img_set, label_set
 
-    def eval(self):
-
-        x = tf.placeholder(tf.float32, [self.batch_size, 512, 128, 3])
-        # x = tf.placeholder(tf.float32, None)
+    def train(self):
+        """train the net"""
+        x = tf.placeholder(tf.float32, [self.batch_size, 128, 512, 3])
         label = tf.placeholder(tf.float32, [self.batch_size, self.label_size])
+        # global_step = tf.placeholder(tf.int32, shape=[1])
+        # predict
         pred = u_net(x)
         loss = eva_loss(label, pred)
 
-        opt = tf.train.AdamOptimizer()
+        # optimizer
+        opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
         op = opt.minimize(loss)
+
+        # initialize
         init = tf.global_variables_initializer()
+        self.sess.run(init)
+
+        batch_train = int(len(self.train_set) / self.batch_size)
+        batch_val = int(len(self.val_set) / self.batch_size)
+
+        saver = tf.train.Saver()
+
+        writer = tf.summary.FileWriter("logs/", self.sess.graph)
+        train_loss_summary = tf.summary.scalar("train_loss", loss)
+        val_loss_summary = tf.summary.scalar("val_loss", loss)
+        image_summary = tf.summary.image("image", x)
+        label_summary = tf.summary.tensor_summary("lable", label)
+        pred_summary = tf.summary.tensor_summary("pred", pred)
+
+        print("train on {0} samples, val on {1} samples".format(len(self.train_set), len(self.val_set)))
 
         for i in range(self.epoch_num):
             self.num = 0
-            for j in range(int(len(self.lines) / self.batch_size)):
-                image_set, label_set = self.load_data()
+            print("Epoch {0}:".format(i))
+
+            # train
+            for j in range(batch_train):
+                train_set, train_label = self.load_data(train_set=self.train_set)
                 # label_set = tf.Variable(label_set,False)
-                image_set = tf_read_image(image_set, sess=self.sess)
-                pred_, loss_, _ = self.sess.run([init, pred, loss, op], feed_dict={x: image_set, label: label_set})
-                print(pred_, loss_)
+                train_set = tf_read_image(train_set, sess=self.sess)
+                pred_, loss_, _, train_loss_summ = self.sess.run(
+                    [pred, loss, op, train_loss_summary],
+                    feed_dict={x: train_set, label: train_label})
+                print("Batch  {0}/{1}: train_loss:{2:.8f}   ".format(j + 1, batch_train, loss_, pred_), end='\n')
+
+            writer.add_summary(train_loss_summ, i)
+
+            # valid
+            val_sum_loss = 0
+            for j in range(batch_val):
+                val_set, val_label = self.load_data(train_set=self.val_set)
+                val_set = tf_read_image(val_set, sess=self.sess)
+                val_loss, val_loss_summ, image_summ, label_summ, pred_summary = self.sess.run(
+                    [loss, val_loss_summary, image_summary, label_summary, pred_summary],
+                    feed_dict={x: val_set, label: val_label})
+                val_sum_loss += val_loss
+            print("Batch  {0}/{1}: train_loss:{2:.8f}  val_loss:{3:.8f}  ".format(batch_train, batch_train, loss_,
+                                                                                  val_sum_loss / batch_val), end='\n')
+            writer.add_summary(val_loss_summ, i)
+            # writer.add_summary(image_summ)
+            # writer.add_summary(label_summ)
+
+            if i % 5 == 4:
+                saver.save(self.sess,
+                           "./model/train_loss_{0:.3f}_val_loss_{1:.3f}".format(loss_, val_sum_loss / batch_val),
+                           i, latest_filename="final_weight")
 
 
 if __name__ == '__main__':
